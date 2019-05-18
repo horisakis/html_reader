6ec18dcb80d93fac76e04a0186fa3c12b28c5810
horisaki <seiji.horisaki@gmail.com>
2019-05-18 19:14:53 +0900

    パーサーを追加

--- reader.js
+++ reader.js
@@ -1,6 +1,9 @@
 var fs = require("fs")
+const jsdom = require("jsdom")
+const { JSDOM } = jsdom
 
 fs.readFile("./sample.html", "utf-8", function (err, data) {
-    console.log(data);
+    const dom = new JSDOM(data);
+    console.log(dom.window.document.querySelector("h1").textContent);
     
 })
